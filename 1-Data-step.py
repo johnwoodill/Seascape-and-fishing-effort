bdd11ce7bd9d60bebaab69d3c1c6f32e95c782b6
Andrew Woodill <johnwoodill@gmail.com>
2019-07-12 14:53:09 -0700

    first cut of EDA

--- 1-Data-step.py
+++ 1-Data-step.py
@@ -8,42 +8,20 @@ from scipy import spatial
 import numpy as np
 from joblib import Parallel, delayed
 import multiprocessing
+from datetime import datetime
 
 
-
-
-lon1 = -68
-lon2 = -51
-lat1 = -48
-lat2 = -39
 
 # Uncomment to reprocess data
 #--------------------------------------------------------------------------------
 
+# lon1 = -68
+# lon2 = -51
+# lat1 = -48
+# lat2 = -39
 
-# # Get global fish watch data
-# GFW_DIR = '/data2/GFW_public/fishing_effort_100d/daily_csvs'
-
-# allFiles = sorted(glob.glob(GFW_DIR + "/*.csv"))
-
-# list_ = []
-# # Append files in subdir
-# for file_ in allFiles:
-#     print(file_)
-#     df = pd.read_csv(file_, index_col=None, header=0, low_memory=False)
-#     df['lat1'] = df['lat_bin']/10
-#     df['lon1'] = df['lon_bin']/10
-#     df['lat2'] = df['lat1'] + 0.010
-#     df['lon2'] = df['lon1'] + 0.010
-#     dat = df[(df['lon1'] >= lon1) & (df['lon2'] <= lon2)] 
-#     dat = dat[(dat['lat1'] >= lat1) & (dat['lat2'] <= lat2)]
-#     list_.append(dat)
-#     outdat = pd.concat(list_, axis = 0, ignore_index = True)
-
-# outdat.to_feather("data/patagonia_shelf_gfw_effort_100d_data.feather")
-
-# # Get seascape data
-# SEASCAPE_DIR = 'data/seascapes'
+# Get seascape data
+# SEASCAPE_DIR = 'data/seascapes/class'
 
 # allFiles = sorted(glob.glob(SEASCAPE_DIR + "/*.csv"))
 
@@ -55,10 +33,44 @@ lat2 = -39
 #     list_.append(df)
 #     outdat = pd.concat(list_, axis = 0, ignore_index = True)
 
-#outdat['date'] = outdat['UTC'].apply(lambda x: datetime.strptime(x, '%Y-%m-%dT12:00:00Z'))
+# outdat['date'] = outdat['UTC'].apply(lambda x: datetime.strptime(x, '%Y-%m-%dT12:00:00Z'))
 
-#outdat.to_feather("data/noaa_seascape_Patagonia_Shelf_2012-2016.feather")
+# outdat = outdat[['date', 'degrees_north', 'degrees_east', 'None']]
+# outdat = outdat[pd.DatetimeIndex(outdat['date']).year < 2017]
 
+# unique_dates = outdat['date'].apply(lambda x: x.strftime("%Y-%m-%d"))
+# unique_dates = unique_dates.unique()
+
+# outdat = outdat.reset_index()
+# outdat.to_feather("data/noaa_seascape_Patagonia_Shelf_2012-2016.feather")
+
+#outdat = pd.read_feather("data/noaa_seascape_Patagonia_Shelf_2012-2016.feather")
+
+
+
+# # Get global fish watch data
+# GFW_DIR = '/data2/GFW_public/fishing_effort_100d/daily_csvs'
+
+# # # allFiles = sorted(glob.glob(GFW_DIR + "/*.csv"))
+
+# list_ = []
+# outdat = pd.DataFrame()
+# # # Append files in subdir
+# for i in range(len(unique_dates)):
+#     file_ = f"{GFW_DIR}/{unique_dates[i]}.csv"
+#     print(file_)
+#     df = pd.read_csv(file_, index_col=None, header=0, low_memory=False)
+#     df['lat1'] = df['lat_bin']/100
+#     df['lon1'] = df['lon_bin']/100
+#     df['lat2'] = df['lat1'] + 0.010
+#     df['lon2'] = df['lon1'] + 0.010
+#     dat = df[(df['lon1'] >= lon1) & (df['lon2'] <= lon2) & (df['lat1'] >= lat1) & (df['lat2'] <= lat2)] 
+#     #dat = dat[(dat['lat1'] >= lat1) & (dat['lat2'] <= lat2)]
+#     list_.append(dat)
+#     outdat = pd.concat(list_, axis = 0, ignore_index = True)
+#     print(unique_dates[i])
+
+# outdat.to_feather("data/patagonia_shelf_gfw_effort_100d_data.feather")
 
 
 #--------------------------------------------------------------------------------
@@ -70,7 +82,7 @@ lat2 = -39
 # lon_bin western edge of grid
 # lat2 northern edge
 # lon2 eastern edge
-gfw = pd.read_feather("data/patagonia_shelf_gfw_effort_data.feather")
+gfw = pd.read_feather("data/patagonia_shelf_gfw_effort_100d_data.feather")
 
 # seascape data
 sea = pd.read_feather("data/noaa_seascape_Patagonia_Shelf_2012-2016.feather")
@@ -96,8 +108,8 @@ sea['day'] = pd.DatetimeIndex(sea['date']).day
 #dat1 = gfw[(gfw['year'] == 2016) & (gfw['month'] == 1) & (gfw['day'] == 1)]
 #dat2 = sea[(sea['year'] == 2016) & (sea['month'] == 1) & (sea['day'] == 1)]
 
-dat1 = gfw[(gfw['year'] == 2016)]
-dat2 = sea[(sea['year'] == 2016)]
+dat1 = gfw[(gfw['year'] != 2016)]
+dat2 = sea[(sea['year'] != 2016)]
 
 
 #len(dat1)
@@ -113,33 +125,46 @@ def dist(lat1, lon1, lat2, lon2):
 
 def find_seascape(lat, lon):
 
+    #lat = -47
+    #lon = -67
+
     lat1 = lat - 1
     lat2 = lat + 1
     lon1 = lon - 1
     lon2 = lon + 1
 
-    indat = dat2[(dat2['degrees_east'] >= lon1) & (dat2['degrees_east'] <= lon2)] 
-    indat = indat[(indat['degrees_north'] >= lat1) & (indat['degrees_north'] <= lat2)]
-
+    indat = dat2[(dat2['degrees_east'].values >= lon1) & (dat2['degrees_east'].values <= lon2) & (dat2['degrees_north'].values >= lat1) & (dat2['degrees_north'].values <= lat2)] 
+    
     distances = indat.apply(
         lambda row: dist(lat, lon, row['degrees_north'], row['degrees_east']), 
         axis=1)
     
     return indat.loc[distances.idxmin(), 'None']
-    
+
+@ray.remote
 def process_days(dat):
     date = dat['date'].iat[0]
+    print(date)
     dat['seascape'] = dat.apply(lambda row: find_seascape(row['lat2'], row['lon2']), axis=1)
     outdat = dat.reset_index(drop=True)
-    outdat.to_feather(f"data/process_CLASS/process_gfw_seascape_CLASS_2016_{date}.feather")
-    print(date)
+    outdat.to_feather(f"data/process_CLASS/process_gfw_seascape_CLASS_{date}.feather")
+    print(f"{date}: COMPLETE")
+
+
 
 gb = dat1.groupby('date')
 days = [gb.get_group(x) for x in gb.groups]
 
-pool = multiprocessing.Pool(40, maxtasksperchild=1)         
-pool.map(process_days, days)
-pool.close()
+
+
+ray.init()
+results = ray.get([process_days.remote(i) for i in days])
+
+
+
+#pool = multiprocessing.Pool(50, maxtasksperchild=1)         
+#pool.map(process_days, days)
+#pool.close()
 
 
 #print("Processing 2016 Seascapes")
--- figures.R
+++ figures.R
@@ -1,6 +1,16 @@
 library(tidyverse)
 library(ggthemes)
 library(feather)
+library(marmap)
+library(ggmap)
+library(viridis)
+library(lubridate)
+library(RColorBrewer)
+library(ggpmisc)
+
+GAPI_Key <- file("~/Projects/Anomalous-IUU-Events-Argentina/Google_api_key.txt", "r")
+GAPI_Key <- readLines(GAPI_Key)
+register_google(key=GAPI_Key)
 
 seascape_labels <- data.frame(seascape = as.factor(seq(1, 33)),
                               nominal = as.factor(c("NORTH ATLANTIC SPRING, ACC TRANSITION",
@@ -35,28 +45,125 @@ seascape_labels <- data.frame(seascape = as.factor(seq(1, 33)),
                                                     "1-30% ICE PRESENT",
                                                     "30-80% MARGINAL ICE","PACK ICE")))
 
-setwd("~/Projects/Seascape-and-fishing-effort/data/process_CLASS/")
 
-file_list <- list.files()
-for (file in file_list){
-  
-  # if the merged dataset doesn't exist, create it
-  if (!exists("dataset")){
-    dataset <- read_feather(file)
-  }
-  
-  # if the merged dataset does exist, append to it
-  if (exists("dataset")){
-    temp_dataset <-read_feather(file)
-    dataset<-rbind(dataset, temp_dataset)
-    rm(temp_dataset)
-  }
-  
+file_list <- list.files("~/Projects/Seascape-and-fishing-effort/data/process_CLASS/", full.names = TRUE)
+
+dataset = data.frame()
+for (i in file_list){
+  indat <- read_feather(i)
+  # print(i)
+  # print(indat$date[1])
+  dataset <- rbind(dataset, indat)
 }
 
-# Aggregate measure
-dat1 <- dataset %>% 
+sea <- as.data.frame(read_feather("../data/noaa_seascape_Patagonia_Shelf_2012-2016.feather"))
+
+names(sea)[5] <- "seascape"
+sea <- filter(sea, !is.na(sea$seascape))
+sea$seascape <- factor(sea$seascape, levels = seq(1, 33), labels = c("NORTH ATLANTIC SPRING, ACC TRANSITION",
+                                                "SUBPOLAR TRANSITION",
+                                                "TROPICAL SUBTROPICAL TRANSITION",
+                                                "WESTERN WARM POOL SUBTROPICAL",
+                                                "SUBTROPICAL GYRE TRANSITION",
+                                                "ACC, NUTRIENT STRESS",
+                                                "TEMPERATE TRANSITION",
+                                                "INDOPACIFIC SUBTROPICAL GYRE",
+                                                "EQUATORIAL TRANSITION",
+                                                "HIGHLY OLIGOTROPHIC SUBTROPICAL GYRE",
+                                                "TROPICAL/SUBTROPICAL UPWELLING",
+                                                "SUBPOLAR",
+                                                "SUBTROPICAL GYRE MESOSCALE INFLUENCED",
+                                                "TEMPERATE BLOOMS UPWELLING",
+                                                "TROPICAL SEAS",
+                                                "MEDITTERANEAN RED SEA",
+                                                "SUBTROPICAL TRANSITION \n LOW NUTRIENT STRESS",
+                                                "MEDITTERANEAN RED SEA",
+                                                "ARTIC/ SUBPOLAR SHELVES",
+                                                "SUBTROPICAL, FRESH INFLUENCED COASTAL",
+                                                "WARM, BLOOMS, HIGH NUTS",
+                                                "ARCTIC LATE SUMMER",
+                                                "FRESHWATER INFLUENCED POLAR SHELVES",
+                                                "ANTARCTIC SHELVES",
+                                                "ICE PACK",
+                                                "ANTARCTIC ICE EDGE",
+                                                "HYPERSALINE EUTROPHIC, \n PERSIAN GULF, RED SEA",
+                                                "ARCTIC ICE EDGE","ANTARCTIC",
+                                                "ICE EDGE  BLOOM",
+                                                "1-30% ICE PRESENT",
+                                                "30-80% MARGINAL ICE","PACK ICE"))
+sea$date <- as.Date(sea$date)
+head(sea)
+sea$day <- day(sea$date)
+sea$month <- month(sea$date)
+sea$year <- year(sea$date)
+
+sea <- left_join(sea, seascape_labels, by = "seascape")
+
+sea <- filter(sea, day == 1 & month == 1 & year == 2016)
+head(sea)
+
+dataset$day <- day(dataset$date)
+dataset$month <- month(dataset$date)
+dataset$year <- year(dataset$date)
+
+gfw <- filter(dataset, day == 1 & month == 1 & year == 2016)
+head(gfw)
+
+gfw$seascape <- factor(gfw$seascape, levels = seq(1, 33), labels = c("NORTH ATLANTIC SPRING, ACC TRANSITION",
+                                                                     "SUBPOLAR TRANSITION",
+                                                                     "TROPICAL SUBTROPICAL TRANSITION",
+                                                                     "WESTERN WARM POOL SUBTROPICAL",
+                                                                     "SUBTROPICAL GYRE TRANSITION",
+                                                                     "ACC, NUTRIENT STRESS",
+                                                                     "TEMPERATE TRANSITION",
+                                                                     "INDOPACIFIC SUBTROPICAL GYRE",
+                                                                     "EQUATORIAL TRANSITION",
+                                                                     "HIGHLY OLIGOTROPHIC SUBTROPICAL GYRE",
+                                                                     "TROPICAL/SUBTROPICAL UPWELLING",
+                                                                     "SUBPOLAR",
+                                                                     "SUBTROPICAL GYRE MESOSCALE INFLUENCED",
+                                                                     "TEMPERATE BLOOMS UPWELLING",
+                                                                     "TROPICAL SEAS",
+                                                                     "MEDITTERANEAN RED SEA",
+                                                                     "SUBTROPICAL TRANSITION \n LOW NUTRIENT STRESS",
+                                                                     "MEDITTERANEAN RED SEA",
+                                                                     "ARTIC/ SUBPOLAR SHELVES",
+                                                                     "SUBTROPICAL, FRESH INFLUENCED COASTAL",
+                                                                     "WARM, BLOOMS, HIGH NUTS",
+                                                                     "ARCTIC LATE SUMMER",
+                                                                     "FRESHWATER INFLUENCED POLAR SHELVES",
+                                                                     "ANTARCTIC SHELVES",
+                                                                     "ICE PACK",
+                                                                     "ANTARCTIC ICE EDGE",
+                                                                     "HYPERSALINE EUTROPHIC, \n PERSIAN GULF, RED SEA",
+                                                                     "ARCTIC ICE EDGE","ANTARCTIC",
+                                                                     "ICE EDGE  BLOOM",
+                                                                     "1-30% ICE PRESENT",
+                                                                     "30-80% MARGINAL ICE","PACK ICE"))
+
+dat0 <- dataset %>% 
+  filter(!is.na(seascape)) %>% 
   group_by(seascape) %>% 
+  summarise(sum_fishing_hours = sum(fishing_hours),
+            sum_obs = n()) %>% 
+  ungroup() %>% 
+  mutate(total_fishing_hours = sum(sum_fishing_hours),
+         total_obs = sum(sum_obs)) %>% 
+  mutate(p_effort_sea = sum_fishing_hours/total_fishing_hours,
+         p_sample_sea = sum_obs/total_obs,
+         likelihood = p_effort_sea/p_sample_sea) %>% 
+  mutate(seascape = as.factor(seascape)) %>% 
+  left_join(seascape_labels, by="seascape") %>% 
+  filter(!is.na(seascape)) %>%
+  mutate(seascape = reorder(seascape, likelihood))
+
+sum(dat0$p_effort_sea)
+sum(dat0$p_sample_sea)
+
+# Annual measure
+dat1 <- dataset %>% 
+  filter(!is.na(seascape)) %>% 
+  group_by(year, seascape) %>% 
   summarise(sum_fishing_hours = sum(fishing_hours),
             sum_obs = sum(n())) %>% 
   ungroup() %>% 
@@ -64,15 +171,16 @@ dat1 <- dataset %>%
          total_obs = sum(sum_obs)) %>% 
   mutate(p_effort_sea = sum_fishing_hours/total_fishing_hours,
          p_sample_sea = sum_obs/total_obs,
-         weighted_effort = p_effort_sea/p_sample_sea) %>% 
+         likelihood = p_effort_sea/p_sample_sea) %>% 
   mutate(seascape = as.factor(seascape)) %>% 
   left_join(seascape_labels, by="seascape") %>% 
-  filter(!is.na(seascape)) %>% 
-  mutate(seascape = reorder(seascape, weighted_effort))
+  filter(!is.na(seascape)) %>%
+  mutate(seascape = reorder(seascape, likelihood))
 
 # Month measure
 dat2 <- dataset %>% 
-  group_by(month, seascape) %>% 
+  filter(!is.na(seascape)) %>% 
+  group_by(year, month, seascape) %>% 
   summarise(sum_fishing_hours = sum(fishing_hours),
             sum_obs = sum(n())) %>% 
   ungroup() %>% 
@@ -80,50 +188,171 @@ dat2 <- dataset %>%
          total_obs = sum(sum_obs)) %>% 
   mutate(p_effort_sea = sum_fishing_hours/total_fishing_hours,
          p_sample_sea = sum_obs/total_obs,
-         weighted_effort = p_effort_sea/p_sample_sea) %>% 
+         likelihood = p_effort_sea/p_sample_sea) %>% 
   mutate(seascape = as.factor(seascape)) %>% 
   left_join(seascape_labels, by="seascape") %>% 
-  filter(!is.na(seascape)) %>% 
-  mutate(seascape = reorder(seascape, weighted_effort),
+  filter(!is.na(seascape)) %>%
+  mutate(seascape = reorder(seascape, likelihood),
          month = factor(month.abb[month], month.abb))
 
 
-
-ggplot(dat1, aes(x=seascape, y=weighted_effort, fill=seascape))  + 
+ggplot(dat0, aes(x=seascape, y=likelihood, fill=seascape))  + 
   theme_tufte(12) +
-  labs(y="Likelihood Raio \n (P(effort) / P(sample))", x="Seascape Category") +
+  labs(y="Likelihood Ratio \n (P(effort) / P(sample))", x="Seascape Category", title = "2012-2016") +
   scale_fill_viridis(discrete = TRUE, direction = -1) +
   geom_bar(stat="identity") + 
   coord_flip() + 
   geom_text(aes(label = nominal), hjust=0, size=2.5) +
   theme(legend.position = "none",
         panel.border = element_rect(colour = "grey", fill=NA, size=1)) +
-  scale_y_continuous(expand = c(0, 0), 
-                   breaks = c(0, 0.25, 0.5, 0.75, 1),
-                   labels = c("0", "0.25", "0.5", "0.75", "1"),
-                   lim=c(0, 1.35)) +
+  ylim(0, 1.75) +
+  annotate(geom = "table", x = 37, y = -0.8, label = list(dat0), 
+           vjust = 1, hjust = 0) +
+  # scale_y_continuous(expand = c(0, 0), 
+  #                  breaks = c(0, 0.25, 0.5, 0.75, 1),
+  #                  labels = c("0", "0.25", "0.5", "0.75", "1"),
+  #                  lim=c(0, 1.35)) +
   geom_hline(yintercept = 1, linetype = "dashed", color = "black") +
-  # ylim(0, 2) +
-  NULL
+NULL
 
-ggsave("~/Projects/Seascape-and-fishing-effort/figures/EDA_weighted_fishing_effort.pdf", width=8, height=4)
+ggsave("~/Projects/Seascape-and-fishing-effort/figures/EDA_aggregate_weighted_fishing_effort.pdf", width=6, height=4)
 
-ggplot(dat2, aes(x=seascape, y=weighted_effort, fill=seascape))  + 
+ggplot(dat1, aes(x=seascape, y=likelihood, fill=seascape))  + 
   theme_tufte(12) +
-  labs(y="Likelihood Raio \n (P(effort) / P(sample))", x="Seascape Category") +
+  labs(y="Likelihood Ratio \n (P(effort) / P(sample))", x="Seascape Category") +
   scale_fill_viridis(discrete = TRUE, direction = -1) +
   geom_bar(stat="identity") + 
   coord_flip() + 
   geom_text(aes(label = nominal), hjust=0, size=2.5) +
   theme(legend.position = "none",
         panel.border = element_rect(colour = "grey", fill=NA, size=1)) +
-  scale_y_continuous(expand = c(0, 0), 
-                     breaks = c(0, 0.25, 0.5, 0.75, 1),
-                     labels = c("0", "0.25", "0.5", "0.75", "1"),
-                     lim=c(0, 2.5)) +
-  geom_hline(yintercept = 1, linetype = "dashed", color = "black") + facet_wrap(~month) +
+  # ylim(0, 6.25) +
+  scale_y_continuous(expand = c(0, 0),
+                   breaks = c(0, 1),
+                   labels = c("0", "1"),
+                   lim=c(0, 6.5)) +
+  geom_hline(yintercept = 1, linetype = "dashed", color = "black") +
+  facet_wrap(~year) +
   NULL
 
-ggsave("~/Projects/Seascape-and-fishing-effort/figures/EDA_monthly_weighted_fishing_effort.pdf", width=10, height=10)
+ggsave("~/Projects/Seascape-and-fishing-effort/figures/EDA_annual_weighted_fishing_effort.pdf", width=10, height=10)
+
+ggplot(dat2, aes(x=seascape, y=likelihood, fill=seascape))  + 
+  theme_tufte(12) +
+  labs(y="Likelihood Ratio \n (P(effort) / P(sample))", x="Seascape Category") +
+  scale_fill_viridis(discrete = TRUE, direction = -1) +
+  geom_bar(stat="identity") + 
+  coord_flip() + 
+  geom_text(aes(label = nominal), hjust=0, size=2.5) +
+  theme(legend.position = "none",
+        panel.border = element_rect(colour = "grey", fill=NA, size=1)) +
+  # scale_y_continuous(expand = c(0, 0), 
+  #                    breaks = c(0, 0.25, 0.5, 0.75, 1),
+  #                    labels = c("0", "0.25", "0.5", "0.75", "1"),
+  #                    lim=c(0, 2.5)) +
+  ylim(0, 5.25) +
+  geom_hline(yintercept = 1, linetype = "dashed", color = "black") + 
+  facet_wrap(~month+year, ncol = 5) +
+  NULL
+
+ggsave("~/Projects/Seascape-and-fishing-effort/figures/EDA_monthly_weighted_fishing_effort.pdf", width=15, height=40)
 
 
+# Map
+bat <- getNOAA.bathy(-68, -51, -48, -39, res = 1, keep = TRUE)
+bat2 <- getNOAA.bathy(-77, -22, -58, -23, res = 1, keep = TRUE)
+
+loc = c(-58, -22)
+map1 <- ggmap(get_map(loc, zoom = 3, maptype='toner-background', color='bw', source='stamen')) + 
+  geom_segment(x=-62, xend=-62, y=-40, yend=-45, color='red') +
+  geom_segment(x=-62, xend=-54, y=-45, yend=-45, color='red') +
+  geom_segment(x=-54, xend=-54, y=-45, yend=-40, color='red') +
+  geom_segment(x=-62, xend=-54, y=-40, yend=-40, color='red') +
+  labs(x=NULL, y=NULL) +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        panel.border = element_rect(colour = "black", fill=NA, size=1)) +
+  NULL
+map1
+
+gfw$seascape = reorder(dat0$seascape, dat0$likelihood)
+
+sea$seascape <- factor(sea$seascape, levels = c("SUBPOLAR TRANSITION",
+                                       "TEMPERATE BLOOMS UPWELLING",
+                                       "HYPERSALINE EUTROPHIC",
+                                       "WARM, BLOOMS, HIGH NUTS",
+                                       "TEMPERATE TRANSITION",
+                                       "SUBTROPICAL TRANSITION LOW NUTRIENT STRESS",
+                                       "SUBPOLAR",
+                                       "TROPICAL SEAS",
+                                       "SUBTROPICAL GYRE MESOSCALE INFLUENCED",
+                                       "TROPICAL SUBTROPICAL TRANSITION",
+                                       "INDOPACIFIC SUBTROPICAL GYRE",
+                                       "TROPICAL/SUBTROPICAL UPWELLING",
+                                       "NORTH ATLANTIC SPRING, ACC TRANSITION"))
+
+map2 <- ggplot(NULL) +
+  # scale_shape_discrete(solid=FALSE) +
+  labs(title = "January 1, 2016") +
+  labs(color='Seascape') +
+  theme_tufte(12) +
+  geom_point(data=sea, aes(x=degrees_east, y=degrees_north, color=seascape)) +
+  # geom_point(data=gfw, aes(x=lon2, y=lat2, color=seascape)) +
+  geom_point(data=gfw, aes(x=lon2, y=lat2), shape=1, color="black", alpha = 0.75) +
+  labs(x=NULL, y=NULL) +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        panel.border = element_rect(colour = "black", fill=NA, size=1),
+        panel.background = element_rect(fill = 'grey', color = "grey")) +
+  # scale_color_viridis(discrete = TRUE, direction = -1) +
+  ylim(min(gfw$lat2), max(gfw$lat2)) +
+  xlim(min(gfw$lon2), max(gfw$lon2)) +
+  scale_color_manual(values = viridis(13), 
+                     labels=c("SUBPOLAR TRANSITION",
+                              "TEMPERATE BLOOMS UPWELLING",
+                              "HYPERSALINE EUTROPHIC",
+                              "WARM, BLOOMS, HIGH NUTS",
+                              "TEMPERATE TRANSITION",
+                              "SUBTROPICAL TRANSITION LOW NUTRIENT STRESS",
+                              "SUBPOLAR",
+                              "TROPICAL SEAS",
+                              "SUBTROPICAL GYRE MESOSCALE INFLUENCED",
+                              "TROPICAL SUBTROPICAL TRANSITION",
+                              "INDOPACIFIC SUBTROPICAL GYRE",
+                              "TROPICAL/SUBTROPICAL UPWELLING",
+                              "NORTH ATLANTIC SPRING, ACC TRANSITION")) +
+  NULL
+
+map2
+
+ggdraw() + draw_plot(map2, 0, 0, height = 1, width = 1) +
+  draw_plot(map1, .326, 0, height = .25, width = .20)
+
+ggsave("~/Projects/Seascape-and-fishing-effort/figures/Patagonia_map.pdf", width=8, height=4)
+
+tbl <- tableGrob(dat0) 
+tbl_plot <- grid.arrange(tbl)
+
+ggsave("~/Projects/Seascape-and-fishing-effort/figures/aggregate_table.pdf", plot = tbl_plot, width = 14, height = 5)
+
+library(stargazer)
+stargazer(dat0, summarise=FALSE)
+
+system("pdfunite Patagonia_map.pdf EDA_aggregate_weighted_fishing_effort.pdf aggregate_table.pdf EDA_annual_weighted_fishing_effort.pdf EDA_monthly_weighted_fishing_effort.pdf Patagonia_fishing_effort_EDA.pdf")
+
+
+map3 <- ggplot(NULL) +
+  # scale_shape_discrete(solid=FALSE) +
+  labs(title = "January 1, 2016") +
+  labs(color='Seascape') +
+  theme_tufte(12) +
+  geom_point(data=filter(sea, seascape == "TEMPERATE BLOOMS UPWELLING"), aes(x=degrees_east, y=degrees_north, color=factor(seascape)))
+map3
