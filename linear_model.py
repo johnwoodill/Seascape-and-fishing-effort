a3df9fa0673e3ec99d50abe7d5fa7bcb03085417
Andrew Woodill <johnwoodill@gmail.com>
2019-08-23 11:30:07 -0700

    addd probit

--- linear_model.py
+++ linear_model.py
@@ -5,6 +5,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import glob
 from sklearn.preprocessing import MinMaxScaler
+from statsmodels.discrete.discrete_model import Logit
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_validate
 
 # files = glob.glob('data/processed/*.feather')
 # files
@@ -20,7 +24,7 @@ from sklearn.preprocessing import MinMaxScaler
 # dat = mdat
 
 # Full processed data
-dat = pd.read_feather('data/full_gfw_effort_model_data_8DAY_2012-01-01_2016-12-26.feather')
+dat = pd.read_feather('data/full_gfw_10d_effort_model_data_8DAY_2012-01-01_2016-12-26.feather')
 dat = dat[dat.geartype == 'drifting_longlines']
 dat = dat.sort_values('date')
 
@@ -28,32 +32,68 @@ dat = dat.sort_values('date')
 len(dat[dat.fishing_hours > 0])/len(dat)
 dat = dat[dat.fishing_hours > 0]
 
+# If illegally operating inside EEZ (!= ARG)
+dat.loc[:, 'illegal'] = np.where(((dat['eez'] == True) & (dat['flag'] != 'ARG')), 1, 0)
+
+# Convert true/false eez to 0/1
+dat.loc[:, 'eez'] = dat.eez.astype('uint8')
+
+sum(dat.illegal)/len(dat)
+
 # Remove 2015 because incomplete (still processing)
 #dat = dat[dat.year <= 2014]
 
 # Linear model
 
 # Get data frame of variables and dummy seascapes
-moddat = dat[['fishing_hours', 'month', 'seascape_class', 'sst', 'chlor_a']].dropna().reset_index(drop=True)
+moddat = dat[['fishing_hours', 'month', 'seascape_class', 'sst', 'chlor_a', 'flag', 'eez', 'illegal']].dropna().reset_index(drop=True)
 
 # Square sst
 moddat['sst_sq'] = moddat['sst']**2
-moddat = moddat[['fishing_hours', 'month', 'seascape_class', 'sst', 'sst_sq', 'chlor_a']]
+moddat = moddat[['fishing_hours', 'month', 'seascape_class', 'sst', 'sst_sq', 'chlor_a', 'flag', 'eez', 'illegal']]
 
 seascape_dummies = pd.get_dummies(moddat['seascape_class']).reset_index(drop=True)
 month_dummies = pd.get_dummies(moddat['month']).reset_index(drop=True)
-moddat = pd.concat([moddat, month_dummies], axis=1)
+flag_dummies = pd.get_dummies(moddat['flag']).reset_index(drop=True)
+moddat = pd.concat([moddat, month_dummies, flag_dummies], axis=1)
+
+
 
 # Get X, y
 y = moddat['fishing_hours'].reset_index(drop=True)
 y = np.log(1 + y)
-moddat = moddat.drop(columns = ['month', 'fishing_hours', 'seascape_class'])
+moddat = moddat.drop(columns = ['month', 'fishing_hours', 'seascape_class', 'flag'])
 X = moddat
+X.columns
 X.head()
+
 y.head()
 
 mod = sm.OLS(y, X).fit()
 mod.summary()
+
+# Illegal logit check
+lX = moddat.drop(columns=['illegal', 'eez'])
+lX = lX.iloc[:, 0:15]
+lX
+
+y = moddat['illegal']
+X_train, X_test, y_train, y_test = train_test_split(lX, y, test_size=0.25)
+
+clf = LogisticRegression().fit(X_train, y_train)
+pred = clf.predict(X_test)
+sum((pred - y_test))/len(y_test)
+len(y_test)
+clf.score(X_test, y_test)
+
+cross_validate(clf, X_test, y_test, cv=5)
+
+
+
+flog = Logit(y_train, X_train).fit()
+flog.summary()
+flog.predict(y_test, X_test)
+
 
 # ----------------------------------
 # EDA
